92312549797eb76f5a29d0eb3aa04c6c9d7406d0
carnival77 <stam0325@gmail.com>
2021-09-02 12:24:21 +0900

    studied lab 4 - Spring Cloud Config Server and Client

---
+++ helloworld/helloworld/src/main/java/io/spring/helloworld/HelloworldApplication.java
@@ -0,0 +1,61 @@
+package io.spring.helloworld;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.extern.slf4j.Slf4j;
+
+@SpringBootApplication
+public class HelloworldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloworldApplication.class, args);
+	}
+
+}
+
+//1.3. Create Spring Cloud Config Application
+//@RestController
+//class HelloWorldController{
+//	
+//	@Value("${message:default message}")
+//	String message;
+//	
+//	@GetMapping("/hello")
+//	public String helloworld() {
+//		return message;
+//	}
+//}
+
+//1.5.1. Change the message value
+//@RestController
+//@RefreshScope
+//class HelloWorldController {
+//
+//	@Value("${message:default message}")
+//	String message;
+//
+//	@GetMapping("/hello")
+//	public String helloworld() {
+//		return message;
+//	}
+//}
+
+
+//1.5.2. Change the logging level
+@RestController
+@Slf4j
+@RefreshScope
+class HelloWorldController {
+	@Value("${message:default message}")
+	String message;
+	@GetMapping("/hello")
+	public String helloworld() {
+		log.debug("DEBUG!!!!"); 
+		return message;
+	}
+}
